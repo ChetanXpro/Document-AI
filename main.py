7034efa57aba85d20ef9f71916432411373e1b4d
chetan <chetan@wow.local>
2023-11-12 12:06:00 +0530

    add main function

--- main.py
+++ main.py
@@ -6,33 +6,40 @@ import json
 
 load_dotenv()
 
-# Schema
-schema = {
-    "properties": {
-        "pdfType": {"type": "string"},
-        "name": {"type": "string"},
-        "email": {"type": "string"},
-        "skills": {"type": "array", "items": {"type": "string"}},
+
+
+def extract_resume():
+    # Schema
+    schema = {
+        "properties": {
+            "pdfType": {"type": "string"},
+            "name": {"type": "string"},
+            "email": {"type": "string"},
+            "skills": {"type": "array", "items": {"type": "string"}},
+        
+        },
+        "required": ["name", "email","pdfType","skills"],
+    }
+
+
+    # Load documents for processing
+    loader = PyPDFLoader("sample_resume.pdf")
+    documents = loader.load()
+
+    # Run chain
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    chain = create_extraction_chain(schema, llm)
+    response=chain.run(documents)[0]
+
+
+    json_object = json.dumps(response , indent=4)
+
+
     
-    },
-    "required": ["name", "email","pdfType","skills"],
-}
+    # Writing to sample.json
+    with open("sample.json", "w") as outfile:
+        outfile.write(json_object)
 
 
-# Load documents for processing
-loader = PyPDFLoader("sample_resume.pdf")
-documents = loader.load()
-
-# Run chain
-llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
-chain = create_extraction_chain(schema, llm)
-response=chain.run(documents)[0]
-
-
-json_object = json.dumps(response , indent=4)
-
-
- 
-# Writing to sample.json
-with open("sample.json", "w") as outfile:
-    outfile.write(json_object)
+if __name__ == "__main__":
+    extract_resume()
